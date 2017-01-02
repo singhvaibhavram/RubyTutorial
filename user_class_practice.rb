bb44c9b19abe449fba75794f9219efcd2a653d85
singhvaibhavram <singh.vaibhav.ram@gmail.com>
2017-01-02 16:54:10 +0530

    New Update

--- user_class_practice.rb
+++ user_class_practice.rb
@@ -1,28 +1,38 @@
 # Creating Class
 # User
 class User
-  
+  attr_accessor :name, :email
   #  Initialize method gets called everytime you create a new object
   def initialize(name)
     # Give name to the user object. @name is an instance variable and it life is as long as the class exists. Created within the class
-    @name = name    
+    @name = name
+    @email = email   
   end
 
   def hello
     puts "hello"
   end
-    
+  
+  def self.identify_yourself
+    puts "Hey I am a class method"
+  end
 
 end
 # Call Class
-user = User.new("Ram")
-user1 = User.new("Rajul")
-user2 = User.new("Sachin")
+#user = User.new("Ram")
+#user1 = User.new("Rajul")
+#user2 = User.new("Sachin")
 
 #puts user
 #puts user1
 #puts user2
 
-puts user.hello
-puts user1.hello
-puts user2.hello
+#puts user.hello
+#puts user1.hello
+#puts user2.hello
+
+user = User.new("Ram", "Ram@example.com")
+
+user.run
+# to run this class method you don't need an instance of user you can directly call the
+User.identify_youself 
