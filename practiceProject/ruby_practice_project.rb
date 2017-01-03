61f45a76bf168aea6e93c09764ab5d89f808a540
singhvaibhavram <singh.vaibhav.ram@gmail.com>
2017-01-03 14:55:33 +0530

    First Commit

---
+++ practiceProject/ruby_practice_project.rb
@@ -0,0 +1,7 @@
+# Build a program that: 
+  # Reads in user information that is provided
+  # Reads in permissions from a json file
+  # reates a new user with the assigned permissions
+  # Saves the user in new user listing file, if file doesn't exist then it creates one
+#
+
